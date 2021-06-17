7af2652ac4f3732624274ef9555ccb39543b06e7
Shreyas <shreyasbabshet89@gmail.com>
2021-06-18 01:19:28 +0530

    new files added for hosting

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +24,7 @@ SECRET_KEY = '*qwvu5+co-z4(@my1c(90h*)cbejnp)%d+k1f645sh(9ul=&07'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','food-ordering-app989.herokuapp.com']
 
 AUTH_USER_MODEL = 'webapp.User'
 # Application definition
