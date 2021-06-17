9217b1ad98f4433e724779c7c45f874c73bd3d99
Shreyas <shreyasbabshet89@gmail.com>
2021-06-17 15:45:04 +0530

    new files are added for hosting

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '*qwvu5+co-z4(@my1c(90h*)cbejnp)%d+k1f645sh(9ul=&07'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','food-ordering-app989.herokuapp.com/']
 
 AUTH_USER_MODEL = 'webapp.User'
 # Application definition
