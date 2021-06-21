409f487f68f3b20259a8fc367d113a6ad1b6971c
Shreyas <shreyasbabshet89@gmail.com>
2021-06-22 00:31:33 +0530

    new files added for hosting

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
