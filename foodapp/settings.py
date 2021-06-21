cd3068b6d856582383437fae99fafa152517c338
Shreyas <shreyasbabshet89@gmail.com>
2021-06-22 00:13:24 +0530

    new files added for hosting

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*qwvu5+co-z4(@my1c(90h*)cbejnp)%d+k1f645sh(9ul=&07'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','food-ordering-app989.herokuapp.com']
 
